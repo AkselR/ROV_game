ed4a2bf166b7a05db6a16b8db2b52e756a570bb2
AkselR <aksel_rasmussen@hotmail.com>
2015-01-31 16:55:42 +0100

    trying list

--- TEST_ROV_SPILL.py
+++ TEST_ROV_SPILL.py
@@ -3,12 +3,15 @@ down='s'
 left='a'
 right='d'
 
+list_size=4
 
+# Creates a list containing 4 lists initialized to 0
+Matrix = [[0 for x in range(list_size)] for x in range(list_size)]
 
 while (1>0):
     #check_keyboard()
 #def check_keyboard():
-    inn=raw_input("hvor vil du \n")
+    inn=raw_input("were do you want to go \n")
 
     if (str(inn)==up):
         print('up')
@@ -20,7 +23,10 @@ while (1>0):
         print('right')
     else:
         print(str('controls are w,a,s, or d (no caps)'))
-
+	
+	
     print('')
     print('------')
+    print(str(Matrix[0][0]))
 
+	
